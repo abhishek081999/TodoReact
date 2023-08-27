1b4568c769a072f9ce45c5d35c6b35e2f588fe83
abhishek081999 <abhishekbhoranj@gmail.com>
2023-08-27 18:44:33 +0530

    use reducers function /hook

--- TodoReactContext+Reducers/src/App.jsx
+++ TodoReactContext+Reducers/src/App.jsx
@@ -2,24 +2,35 @@
 import './App.css'
 import TodoList from './components/TodoList/TodoList';
 import AddTodo from './components/AddTodo/AddTodo';
-import {  useState } from "react"
+import {  useReducer, useState } from "react"
 import TodoContext from './context/TodoContext';
+import todoReducer from './reducers/todoReducer';
+import TodoDispatchContext from './context/TodoDispatchContext';
+
 function App() {
-  const [list,setList] = useState([
-    {
-       id:1, todoData: 'todo 1' , finished:false
-    },
-    {
-        id:2, todoData: 'todo 2', finished:false
-     }
-  ]);
+  // const [list,setList] = useState([
+  //   {
+  //      id:1, todoData: 'todo 1' , finished:false
+  //   },
+  //   {
+  //       id:2, todoData: 'todo 2', finished:false
+  //    }
+  // ]);
+
+  const [list,dispatch] =useReducer(todoReducer,[])
   return (
-    <TodoContext.Provider value={{list,setList}}>
-      <AddTodo updateList={(todo) =>setList([
-       ...list,{id: list.length +1 ,todoData: todo, finished:false}
-       ])} />
+    
+    <TodoContext.Provider value={{list}}>
+      <TodoDispatchContext.Provider value={{dispatch}}>
+      <AddTodo 
+      // updateList={(todo) =>{
+        // dispatch({type: 'add_todo',payload:{todoText: todo}})
+      // }} 
+      />
       <TodoList/>
+      </TodoDispatchContext.Provider>
     </TodoContext.Provider>
+   
   )
 }
 
